26d35b39336c457cb96e6452266e494f8a1b1a7c
Sameer Swankar <sameerswankar96@gmail.com>
2024-07-25 15:24:33 +0530

    added arrays

---
+++ Arrays/App.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+public class App {
+    public static void main(String[] args) {
+        int arr[] = {1,3,34};
+        arr[0] = 0;
+        for(int i:arr)
+        {
+            System.out.println(i);
+        }
+    }
+}
